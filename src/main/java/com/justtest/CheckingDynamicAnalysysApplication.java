4cbc8e4f30186141c92c746f326f76a0bbac9a06
pushpendra <pushpendrasen2520@gmail.com>
2022-01-21 18:35:37 +0530

    testing

---
+++ src/main/java/com/justtest/CheckingDynamicAnalysysApplication.java
@@ -0,0 +1,13 @@
+package com.justtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheckingDynamicAnalysysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CheckingDynamicAnalysysApplication.class, args);
+	}
+
+}
